jan94/ziti

Add indexes for authenticator username and fingerprint. Fixes #3219 Also use  externalId index in IdentityManager.

controller/model/authenticator_manager.go
@@ -22,6 +22,9 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
+	"strings"
+	"time"
+
 	"github.com/google/uuid"
 	"github.com/michaelquigley/pfxlog"
 	"github.com/openziti/foundation/v2/errorz"
@@ -41,9 +44,6 @@ import (
 	"go.etcd.io/bbolt"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
-	"reflect"
-	"strings"
-	"time"
 )
 
 const updateUnrestricted = 1
@@ -241,47 +241,27 @@ func (self *AuthenticatorManager) getRootPool() *x509.CertPool {
 }
 
 func (self *AuthenticatorManager) ReadByUsername(username string) (*Authenticator, error) {
-	query := fmt.Sprintf("%s = \"%v\"", db.FieldAuthenticatorUpdbUsername, username)
-
-	entity, err := self.readEntityByQuery(query)
-
-	if err != nil {
+	modelEntity := &Authenticator{}
+	index := self.env.GetStores().Authenticator.GetUsernameIndex()
+	if err := self.readEntityWithIndex("name", []byte(username), index, modelEntity); err != nil {
+		if boltz.IsErrNotFoundErr(err) {
+			return nil, nil
+		}
 		return nil, err
 	}
-
-	if entity == nil {
-		return nil, nil
-	}
-
-	authenticator, ok := entity.(*Authenticator)
-
-	if !ok {
-		return nil, fmt.Errorf("could not cast from %v to authenticator", reflect.TypeOf(entity))
-	}
-
-	return authenticator, nil
+	return modelEntity, nil
 }
 
 func (self *AuthenticatorManager) ReadByFingerprint(fingerprint string) (*Authenticator, error) {
-	query := fmt.Sprintf("%s = \"%v\"", db.FieldAuthenticatorCertFingerprint, fingerprint)
-
-	entity, err := self.readEntityByQuery(query)
-
-	if err != nil {
+	modelEntity := &Authenticator{}
+	index := self.env.GetStores().Authenticator.GetFingerprintIndex()
+	if err := self.readEntityWithIndex("name", []byte(fingerprint), index, modelEntity); err != nil {
+		if boltz.IsErrNotFoundErr(err) {
+			return nil, nil
+		}
 		return nil, err
 	}
-
-	if entity == nil {
-		return nil, nil
-	}
-
-	authenticator, ok := entity.(*Authenticator)
-
-	if !ok {
-		return nil, fmt.Errorf("could not cast from %v to authenticator", reflect.TypeOf(entity))
-	}
-
-	return authenticator, nil
+	return modelEntity, nil
 }
 
 func (self *AuthenticatorManager) UpdateSelf(authenticatorSelf *AuthenticatorSelf, ctx *change.Context) error {

controller/model/identity_manager.go
@@ -20,6 +20,9 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"sync"
+	"time"
+
 	"github.com/michaelquigley/pfxlog"
 	"github.com/openziti/channel/v4"
 	"github.com/openziti/foundation/v2/errorz"
@@ -41,8 +44,6 @@ import (
 	cmap "github.com/orcaman/concurrent-map/v2"
 	"go.etcd.io/bbolt"
 	"google.golang.org/protobuf/proto"
-	"sync"
-	"time"
 )
 
 const (
@@ -533,25 +534,15 @@ func (self *IdentityManager) VisitIdentityAuthenticatorFingerprints(tx *bbolt.Tx
 }
 
 func (self *IdentityManager) ReadByExternalId(externalId string) (*Identity, error) {
-	query := fmt.Sprintf("%s = \"%v\"", db.FieldIdentityExternalId, externalId)
-
-	entity, err := self.readEntityByQuery(query)
-
-	if err != nil {
+	modelEntity := &Identity{}
+	index := self.env.GetStores().Identity.GetExternalIdIndex()
+	if err := self.readEntityWithIndex("name", []byte(externalId), index, modelEntity); err != nil {
+		if boltz.IsErrNotFoundErr(err) {
+			return nil, nil
+		}
 		return nil, err
 	}
-
-	if entity == nil {
-		return nil, nil
-	}
-
-	identity, ok := entity.(*Identity)
-
-	if !ok {
-		return nil, fmt.Errorf("could not cast from %T to %T", entity, identity)
-	}
-
-	return identity, nil
+	return modelEntity, nil
 }
 
 func (self *IdentityManager) Disable(identityId string, duration time.Duration, ctx *change.Context) error {

petarpetrov216/openziti-petrov

controller/model/authenticator_manager.go
@@ -22,6 +22,9 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
+	"strings"
+	"time"
+
 	"github.com/google/uuid"
 	"github.com/michaelquigley/pfxlog"
 	"github.com/openziti/foundation/v2/errorz"
@@ -41,9 +44,6 @@ import (
 	"go.etcd.io/bbolt"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
-	"reflect"
-	"strings"
-	"time"
 )
 
 const updateUnrestricted = 1
@@ -241,47 +241,27 @@ func (self *AuthenticatorManager) getRootPool() *x509.CertPool {
 }
 
 func (self *AuthenticatorManager) ReadByUsername(username string) (*Authenticator, error) {
-	query := fmt.Sprintf("%s = \"%v\"", db.FieldAuthenticatorUpdbUsername, username)
-
-	entity, err := self.readEntityByQuery(query)
-
-	if err != nil {
+	modelEntity := &Authenticator{}
+	index := self.env.GetStores().Authenticator.GetUsernameIndex()
+	if err := self.readEntityWithIndex("username", []byte(username), index, modelEntity); err != nil {
+		if boltz.IsErrNotFoundErr(err) {
+			return nil, nil
+		}
 		return nil, err
 	}
-
-	if entity == nil {
-		return nil, nil
-	}
-
-	authenticator, ok := entity.(*Authenticator)
-
-	if !ok {
-		return nil, fmt.Errorf("could not cast from %v to authenticator", reflect.TypeOf(entity))
-	}
-
-	return authenticator, nil
+	return modelEntity, nil
 }
 
 func (self *AuthenticatorManager) ReadByFingerprint(fingerprint string) (*Authenticator, error) {
-	query := fmt.Sprintf("%s = \"%v\"", db.FieldAuthenticatorCertFingerprint, fingerprint)
-
-	entity, err := self.readEntityByQuery(query)
-
-	if err != nil {
+	modelEntity := &Authenticator{}
+	index := self.env.GetStores().Authenticator.GetFingerprintIndex()
+	if err := self.readEntityWithIndex("fingerprint", []byte(fingerprint), index, modelEntity); err != nil {
+		if boltz.IsErrNotFoundErr(err) {
+			return nil, nil
+		}
 		return nil, err
 	}
-
-	if entity == nil {
-		return nil, nil
-	}
-
-	authenticator, ok := entity.(*Authenticator)
-
-	if !ok {
-		return nil, fmt.Errorf("could not cast from %v to authenticator", reflect.TypeOf(entity))
-	}
-
-	return authenticator, nil
+	return modelEntity, nil
 }
 
 func (self *AuthenticatorManager) UpdateSelf(authenticatorSelf *AuthenticatorSelf, ctx *change.Context) error {

controller/model/identity_manager.go
@@ -20,6 +20,9 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"sync"
+	"time"
+
 	"github.com/michaelquigley/pfxlog"
 	"github.com/openziti/channel/v4"
 	"github.com/openziti/foundation/v2/errorz"
@@ -41,8 +44,6 @@ import (
 	cmap "github.com/orcaman/concurrent-map/v2"
 	"go.etcd.io/bbolt"
 	"google.golang.org/protobuf/proto"
-	"sync"
-	"time"
 )
 
 const (
@@ -533,25 +534,15 @@ func (self *IdentityManager) VisitIdentityAuthenticatorFingerprints(tx *bbolt.Tx
 }
 
 func (self *IdentityManager) ReadByExternalId(externalId string) (*Identity, error) {
-	query := fmt.Sprintf("%s = \"%v\"", db.FieldIdentityExternalId, externalId)
-
-	entity, err := self.readEntityByQuery(query)
-
-	if err != nil {
+	modelEntity := &Identity{}
+	index := self.env.GetStores().Identity.GetExternalIdIndex()
+	if err := self.readEntityWithIndex("externalId", []byte(externalId), index, modelEntity); err != nil {
+		if boltz.IsErrNotFoundErr(err) {
+			return nil, nil
+		}
 		return nil, err
 	}
-
-	if entity == nil {
-		return nil, nil
-	}
-
-	identity, ok := entity.(*Identity)
-
-	if !ok {
-		return nil, fmt.Errorf("could not cast from %T to %T", entity, identity)
-	}
-
-	return identity, nil
+	return modelEntity, nil
 }
 
 func (self *IdentityManager) Disable(identityId string, duration time.Duration, ctx *change.Context) error {