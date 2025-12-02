openziti/ziti

Optimize router data model subscription code. Add additional events. Fixes #3359

controller/model/identity_manager.go
@@ -452,10 +452,10 @@ func (self *IdentityManager) ApplyUpdateServiceConfigs(cmd *UpdateServiceConfigs
 					serviceMap = map[string]string{}
 					identity.ServiceConfigs[serviceConfig.Service] = serviceMap
 				}
-				serviceMap[config.Type] = config.Id
+				serviceMap[config.TypeId] = config.Id
 			} else if identity.ServiceConfigs != nil {
 				if serviceMap, ok := identity.ServiceConfigs[serviceConfig.Service]; ok {
-					delete(serviceMap, config.Type)
+					delete(serviceMap, config.TypeId)
 				}
 			}
 		}

petarpetrov216/openziti-petrov

controller/model/identity_manager.go
@@ -452,10 +452,10 @@ func (self *IdentityManager) ApplyUpdateServiceConfigs(cmd *UpdateServiceConfigs
 					serviceMap = map[string]string{}
 					identity.ServiceConfigs[serviceConfig.Service] = serviceMap
 				}
-				serviceMap[cfg.Type] = cfg.Id
+				serviceMap[cfg.TypeId] = cfg.Id
 			} else if identity.ServiceConfigs != nil {
 				if serviceMap, ok := identity.ServiceConfigs[serviceConfig.Service]; ok {
-					delete(serviceMap, cfg.Type)
+					delete(serviceMap, cfg.TypeId)
 				}
 			}
 		}